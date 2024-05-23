5ee434d8d951eaa540d7fcf19d5136205a0e1396
Nayan Kumar <ind.nayan4729@gmail.com>
2024-05-23 17:40:46 +0530

    Getting thumbnails using mongoose virtuals

--- models/campground.js
+++ models/campground.js
@@ -5,14 +5,19 @@ const { cloudinary } = require('../cloudinary');
 
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+  url: String,
+  filename: String
+});
+
+ImageSchema.virtual('thumbnail')
+  .get(function () {
+    return this.url.replace('/upload', '/upload/w_400,h_300');
+  });
+
 const CampgroundSchema = new Schema({
   title: String,
-  images: [
-    {
-      url: String,
-      filename: String
-    }
-  ],
+  images: [ ImageSchema ],
   price: Number,
   description: String,
   location: String,
